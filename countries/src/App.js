7437987fceb1262cf371347582dcb9126aaa2c80
Javier Monardo <javiermonardo@weplan-latam.com>
2021-08-10 22:00:13 -0300

    Structure of the project made and developing Header component

---
+++ countries/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./Helpers/history";
+import Home from "./Components/Home";
+
+const  App = () => {
+
+
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ countries/src/Components/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+const Home = () => {
+
+
+    return (
+        <div className="main-container-dark">
+            <header className="hdDark">
+                <div id="home-flex-container">
+                    <div className="title">
+                        Where in the world?
+                    </div>
+                    <div className="mode">
+                        Dark Mode
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+
+export default Home;
